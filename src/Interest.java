8fbe72d93bb6e8e4d375a1bca1f4304781c56818
Admin <info@rivitech.co>
2023-02-24 22:32:02 +0530

    Bala change

--- src/Interest.java
+++ src/Interest.java
@@ -1,20 +1,17 @@
 
-public class Interest {
-public static void main(String[] args)
-{
-int p=1000, t=3, r;
-System.out.println(t>5);
-System.out.println(t<10);
-if(t<5){
-r=20;
-}
-else if(t>5 && t<10) {
-	r=10;
-}
-else {
-r=5;
-}
-int interest=(p*t*r)/100;
-System.out.println(interest);
-}
+class Bank {
+	public static void main(String[] args) {
+		int p = 1000, t = 3, r;
+		System.out.println(t > 5);
+		System.out.println(t < 10);
+		if (t < 5) {
+			r = 20;
+		} else if (t > 5 && t < 10) {
+			r = 10;
+		} else {
+			r = 5;
+		}
+		int interest = (p * t * r) / 100;
+		System.out.println(interest);
+	}
 }
