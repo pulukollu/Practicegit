71cd2170da849e8877110bdcd170c62d8cfe07a7
Pulukollu Github <vpulukollu@gmail.com>
2023-02-25 09:39:36 -0600

    did changes from line 7 - 9

--- src/Interest.java
+++ src/Interest.java
@@ -4,17 +4,9 @@ class Banks {
 		return 10;
 	}
 	public static void main(String[] args) {
-		int p = 1000, t = 3, r;
-		System.out.println(t > 5);
-		System.out.println(t < 10);
-		if (t < 5) {
-			r = 20;
-		} else if (t > 5 && t < 10) {
-			r = 10;
-		} else {
-			r = 5;
-		}
-		int interest = (p * t * r) / 100;
-		System.out.println(interest); 
+		float x=10.5f;
+		float a = Float.valueOf(x);
+		System.out.println(a);
+		
 	}
 }
