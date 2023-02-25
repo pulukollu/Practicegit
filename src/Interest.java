225002782eb51ccd71cad0418cc8b213fe89015f
Pulukollu Github <vpulukollu@gmail.com>
2023-02-25 09:46:39 -0600

    editing updated

--- src/Interest.java
+++ src/Interest.java
@@ -1,25 +1,8 @@
 
 class Banks {
-	public static int go() {
-		return 10;
-	}
-
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		float x = 10.5f;
 		float a = Float.valueOf(x);
 		System.out.println(a);
-		int p = 1000, t = 3, r;
-		System.out.println(t > 5);
-		System.out.println(t < 10);
-		if (t < 5) {
-			r = 20;
-		} else if (t > 5 && t < 10) {
-			r = 10;
-		} else {
-			r = 5;
 		}
-		int interest = (p * t * r) / 100;
-		System.out.println(interest);
-		go();
-	}
 }
